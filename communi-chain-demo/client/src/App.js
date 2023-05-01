2c09f2ada0a2b9a1f9efda5e9b852ab36a182079
jeslynlamxy <jeslynlxy@hotmail.com>
2023-05-02 00:50:08 +0800

    meta tests

--- communi-chain-demo/client/src/App.js
+++ communi-chain-demo/client/src/App.js
@@ -7,9 +7,53 @@ import { Toaster } from "react-hot-toast";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useSelector } from "react-redux";
 import Loader from "./components/Loader";
+import React, { useEffect } from "react";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import { ethers } from "ethers";
 
 function App() {
-  const {loader} = useSelector(state => state.loaderReducer)
+  
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Please install MetaMask!");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      fetchGreetings();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchGreetings = async () => {
+    let contractAddress = "YOUR_CONTRACT_ADDRESS";
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert("Please install MetaMask!");
+      return;
+    }
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      contractAddress,
+      Greeter.abi,
+      provider
+    );
+    const greeting = await contract.greet();
+    console.log(greeting);
+  };
+
+  useEffect(() => {
+    connectWallet();
+  }, []);
+
+  const { loader } = useSelector(state => state.loaderReducer)
+  
   return (
     <div>
       <Toaster position="top-center" reverseOrder={false} />
