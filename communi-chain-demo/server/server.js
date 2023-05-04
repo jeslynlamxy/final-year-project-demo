42b13892fe9fcf9f97b779b82d7e8a5d90deb39a
jeslynlamxy <jeslynlxy@hotmail.com>
2023-05-04 19:49:44 +0800

    test

--- communi-chain-demo/server/server.js
+++ communi-chain-demo/server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const dbConfig = require("./config/dbConfig");
+require("./config/dbConfig");
 const port = process.env.PORT || 4999;
 
 const usersRoute = require("./routes/usersRoute");
