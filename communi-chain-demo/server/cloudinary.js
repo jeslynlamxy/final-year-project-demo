f81e2196790e74b9e54bdda417460f8da98a6e1a
jieshilyn <39185220+jeslynlamxy@users.noreply.github.com>
2023-05-07 15:51:35 +0800

    Delete cloudinary.js

--- communi-chain-demo/server/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: "dznporaco",
-  api_key: "772398539171842",
-  api_secret: "9SPdAVPop0kaPFVADK7mAEUqurM",
-});
-
-module.exports = cloudinary;
